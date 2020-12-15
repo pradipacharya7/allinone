3c19658072eca084b644adf23e5790c3db1fea94
pradipacharya7 <pradip.acharya077@gmail.com>
2020-12-15 17:25:23 -0500

    Tests added for Rest Controler

--- src/main/java/com/allinone/practice/REST/StudentResources.java
+++ src/main/java/com/allinone/practice/REST/StudentResources.java
@@ -1,27 +1,45 @@
 package com.allinone.practice.REST;
 
 import com.allinone.practice.model.Student;
-import com.allinone.practice.restexception.ResourceNotFoundException;
 import com.allinone.practice.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("student/")
+@Validated
 public class StudentResources {
     @Autowired
     StudentService studentService;
 
     @RequestMapping("/show")
     public List<Student> showall() throws Exception {
-        List<Student> students=null;
+        List<Student> students=studentService.findAll();
         if(students==null){
             throw new Exception("Not Found");
         }
         return students;
+    }
+    @RequestMapping("/delete/{id}")
+    public Student deleteById(@PathVariable("id") int id) throws Exception {
+        Student student=studentService.findById(id);
+        if(student==null){
+            throw new Exception("No such student with id:" +id+" found");
+        }
+     return studentService.deleteById(id);
+    }
+
+    @RequestMapping(value = "/save",method = RequestMethod.POST, produces = "application/json")
+    public Student save(@Valid @RequestBody Student student) throws Exception{
+        System.out.println("");
+        Student student1=studentService.save(student);
+        if(student1==null){
+             new Exception("Failed to save Student.. Try Again");
+        }
+         return student1;
     }
 }
--- src/test/java/com/allinone/practice/REST/StudentResourcesTest.java
+++ src/test/java/com/allinone/practice/REST/StudentResourcesTest.java
@@ -11,6 +11,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Arrays;
@@ -29,15 +30,28 @@ public class StudentResourcesTest {
     Student student1,student2;
     List<Student> students;
     @Before
-    public void setUp() throws Exception {
+    public void setUp(){
         student1=new Student(1,"Pradip","pradip.acahry@gmail.com",1);
-        student2=new Student(1,"Prasiddha","pradiddha.acahry@gmail.com",2);
+        student2=new Student(2,"Prasiddha","pradiddha.acahry@gmail.com",2);
         students=Arrays.asList(student1,student2);
     }
 
     @Test
-    public  void showAll() throws Exception{
+    public  void showAll() throws Exception {
         Mockito.when(studentService.findAll()).thenReturn(Stream.of(student1,student2).collect(Collectors.toList()));
         Assertions.assertThat(2).isEqualTo(studentResources.showall().size());
     }
+
+//    @Test
+//    public void deleteById() throws Exception{
+//
+//        Mockito.when(studentService.findById(2)).thenReturn(student2);
+//        Assertions.assertThat(student2).isEqualTo(studentResources.deleteById(2));
+//    }
+    @Test
+    public void save() throws Exception{
+        Mockito.when(studentService.save(student1)).thenReturn(student1);
+        Assertions.assertThat(student1).isEqualTo(studentResources.save(student1));
+    }
+
 }
